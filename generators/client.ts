889530c38a0bcb7e019abb7eb00a236620e363be
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-08-29 20:43:48 -0400

    chore: setup project

---
+++ generators/client.ts
@@ -0,0 +1,6 @@
+import { defineGenerator } from '@alloc/codegentool'
+
+export type ClientOptions = {}
+
+export const generateClient = (options: ClientOptions = {}) =>
+  defineGenerator(async api => {})
