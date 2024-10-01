ed9f7b21d5c97d2a02aeed859147d27c071e88a7
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-10-01 13:10:29 -0400

    chore: remove unused Tuple.isTuple static method

--- packages/pg-native/src/tuple.ts
+++ packages/pg-native/src/tuple.ts
@@ -1,6 +1,1 @@
-// biome-ignore lint/complexity/noStaticOnlyClass:
-export class Tuple<T = unknown> extends Array<T> {
-  static isTuple(value: unknown): value is Tuple {
-    return value != null && value.constructor === Tuple
-  }
-}
+export class Tuple<T = unknown> extends Array<T> {}
