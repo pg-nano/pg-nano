7a5a3977b58ccf429b688d918aaec36a574835d5
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-10-01 14:37:17 -0400

    test: add basic `generate` tests

---
+++ test/node/generate.test.ts
@@ -0,0 +1,49 @@
+import { createProject, dedent } from '../util.js'
+
+const sql = dedent
+
+describe('generate', () => {
+  test('zero argument routine', async () => {
+    const project = await createProject({
+      'sql/schema.sql': sql`
+        CREATE FUNCTION forty_two() RETURNS int AS $$
+        BEGIN
+          RETURN 42;
+        END;
+        $$ LANGUAGE plpgsql;
+      `,
+    })
+
+    await project.generate()
+    expect(project.generatedFiles).toMatchSnapshot()
+
+    type Schema =
+      typeof import('../__fixtures__/generate__zero_argument_routine/sql/schema.ts')
+
+    const client = await project.importClient<Schema>()
+
+    expect(await client.fortyTwo()).toBe(42)
+  })
+
+  test('one argument routine', async () => {
+    const project = await createProject({
+      'sql/schema.sql': sql`
+        CREATE FUNCTION multiply_by_2(x int) RETURNS int AS $$
+        BEGIN
+          RETURN x * 2;
+        END;
+        $$ LANGUAGE plpgsql;
+      `,
+    })
+
+    await project.generate()
+    expect(project.generatedFiles).toMatchSnapshot()
+
+    type Schema =
+      typeof import('../__fixtures__/generate__one_argument_routine/sql/schema.ts')
+
+    const client = await project.importClient<Schema>()
+
+    expect(await client.multiplyBy2(21)).toBe(42)
+  })
+})
