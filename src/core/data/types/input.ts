d801a04d0eaa838889afc9a9b48cd638d410b62a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-23 13:41:40 -0500

    fix: preserve JSON type passed into `Input<T>`

--- src/core/data/types/input.ts
+++ src/core/data/types/input.ts
@@ -1,3 +1,4 @@
+import type { JSON } from './json'
 import type { Timestamp } from './timestamp'
 
 type IsTimestamp<T> = 'Timestamp' extends Extract<T, Timestamp>['__brand']
@@ -8,19 +9,21 @@ type IsTimestamp<T> = 'Timestamp' extends Extract<T, Timestamp>['__brand']
  * Certain types have implicit coercion applied when passed to a Postgres
  * routine. This type widens type `T` recursively to allow coercible types.
  */
-export type Input<T> = T extends (...args: any[]) => any
-  ? T
-  : T extends readonly (infer TElement)[]
-    ? TElement[] extends T
-      ? readonly Input<TElement>[]
-      : // Avoid widening tuples to arrays.
-        { [Index in keyof T]: Input<T[Index]> }
-    : IsTimestamp<T> extends true
-      ? Date | number
-      : T extends undefined
-        ? T | null | undefined
-        : T extends BigInt
-          ? T | number
-          : T extends object
-            ? { [K in keyof T]: Input<T[K]> }
-            : T
+export type Input<T> = JSON extends T
+  ? JSON
+  : T extends (...args: any[]) => any
+    ? T
+    : T extends readonly (infer TElement)[]
+      ? TElement[] extends T
+        ? readonly Input<TElement>[]
+        : // Avoid widening tuples to arrays.
+          { [Index in keyof T]: Input<T[Index]> }
+      : IsTimestamp<T> extends true
+        ? Date | number
+        : T extends undefined
+          ? T | null | undefined
+          : T extends BigInt
+            ? T | number
+            : T extends object
+              ? { [K in keyof T]: Input<T[K]> }
+              : T
