27752caaf6d7037d3f22841e361e984b4c770bf4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-12-17 14:03:44 -0500

    feat(client): add `toTimestamp` helper function

--- src/core/data/types/timestamp.ts
+++ src/core/data/types/timestamp.ts
@@ -1,3 +1,6 @@
+import { isDate } from 'radashi'
+import type { Input } from './input.js'
+
 /**
  * Timestamps are represented in milliseconds (1e-3 seconds) since the Unix
  * epoch.
@@ -7,3 +10,14 @@
  * [#48](https://github.com/pg-nano/pg-nano/issues/48).
  */
 export type Timestamp = number & { __brand?: 'Timestamp' }
+
+/**
+ * Casts an input to a `Timestamp`.
+ *
+ * Note: This is only useful when your application server needs to patch a
+ * database result using data retrieved from elsewhere. When calling a Postgres
+ * routine, timestamp inputs are casted automatically.
+ */
+export function toTimestamp(input: Input<Timestamp>): Timestamp {
+  return isDate(input) ? input.getTime() : input
+}
