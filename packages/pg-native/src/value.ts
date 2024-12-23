5e1d3c4799f336ad342771f1735157345affc731
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-12-23 16:41:38 -0500

    fix(pg-native): escaped literals within row/array literals

--- packages/pg-native/src/value.ts
+++ packages/pg-native/src/value.ts
@@ -4,113 +4,226 @@ import { Range, serialize as stringifyRange } from 'postgres-range'
 import { isFunction } from 'radashi'
 import { Tuple } from './tuple.js'
 
-const noEscape = <T>(x: T) => x
+// Tuple-marked objects exist to simplify pg-native tests. They are object
+// literals that stringifyValue will render as a row literal (AKA a tuple).
+export const tupleMarkedObjects = new WeakSet<object>()
 
-type Escape = (value: string, type: EscapedType) => string
-type EscapedType =
-  | 'array'
-  | 'date'
-  | 'interval'
-  | 'hex'
-  | 'json'
-  | 'pattern'
-  | 'range'
-  | 'string'
-  | 'tuple'
+// biome-ignore lint/suspicious/noConstEnum:
+// biome-ignore lint/style/useEnumInitializers:
+const enum DataType {
+  Nullish,
+  String,
+  Number,
+  Boolean,
+  BigInt,
+  Array,
+  Tuple,
+  TupleObject,
+  Interval,
+  Range,
+  Date,
+  TypedArray,
+  Buffer,
+  Json,
+}
+
+const nonEscapedTypes = new Set([
+  DataType.Nullish,
+  DataType.Number,
+  DataType.Boolean,
+  DataType.BigInt,
+])
+
+export type Escape = (str: string, type: DataType, quoteDepth: number) => string
 
 export function stringifyValue(
   value: unknown,
-  escape: Escape = noEscape,
+  escape?: Escape | null,
+  parentType?: DataType,
+  quoteDepth = 0,
 ): string {
+  const type = reflectType(value)
+  const result = stringifyTypedValue(value, type, parentType, quoteDepth)
+  if (escape && !nonEscapedTypes.has(type)) {
+    return escape(result, type, quoteDepth)
+  }
+  return result
+}
+
+function stringifyArray(
+  array: any[],
+  parentType: DataType | undefined,
+  quoteDepth: number,
+) {
+  // When an array literal is nested in another array literal, only the
+  // outermost array literal should be wrapped in double quotes.
+  const escapeRequired = parentType != null && parentType !== DataType.Array
+
+  if (escapeRequired) {
+    quoteDepth++
+  }
+
+  let str = '{'
+  for (let i = 0; i < array.length; i++) {
+    if (i > 0) {
+      str += ','
+    }
+    str += stringifyValue(array[i], escapeElement, DataType.Array, quoteDepth)
+  }
+  str += '}'
+
+  if (escapeRequired) {
+    return quoteWrap(str, quoteDepth - 1)
+  }
+  return str
+}
+
+function stringifyTuple(
+  tuple: Tuple,
+  parentType: DataType | undefined,
+  quoteDepth: number,
+) {
+  const escapeRequired = parentType != null
+  
+  if (escapeRequired) {
+    quoteDepth++
+  }
+
+  let str = '('
+  for (let i = 0; i < tuple.length; i++) {
+    if (i > 0) {
+      str += ','
+    }
+    // NULL values can be omitted from the tuple literal.
+    if (tuple[i] !== null) {
+      str += stringifyValue(tuple[i], escapeElement, DataType.Tuple, quoteDepth)
+    }
+  }
+  str += ')'
+
+  if (escapeRequired) {
+    return quoteWrap(str, quoteDepth - 1)
+  }
+  return str
+}
+
+const BACKSLASH_RE = /\\/g
+const DOUBLE_QUOTE_RE = /"/g
+
+// Used by array literals and tuple literals.
+function escapeElement(str: string, type: DataType, quoteDepth: number) {
+  switch (type) {
+    case DataType.String:
+    case DataType.Json: {
+      const escape = '\\'.repeat(2 ** (quoteDepth + 1) - 1)
+      return quoteWrap(
+        str
+          .replace(BACKSLASH_RE, escape + '\\')
+          .replace(DOUBLE_QUOTE_RE, escape + '"'),
+        quoteDepth,
+      )
+    }
+  }
+  return str
+}
+
+function stringifyTypedValue(
+  value: unknown,
+  type: DataType,
+  parentType: DataType | undefined,
+  quoteDepth: number,
+) {
+  switch (type) {
+    case DataType.Nullish:
+      return 'null'
+    case DataType.String:
+      return value as string
+    case DataType.Number:
+    case DataType.Boolean:
+    case DataType.BigInt:
+      return (value as number | boolean | bigint).toString()
+    case DataType.Array:
+      return stringifyArray(value as any[], parentType, quoteDepth)
+    case DataType.Tuple:
+      return stringifyTuple(value as Tuple, parentType, quoteDepth)
+    case DataType.Json:
+      return JSON.stringify(value)
+    case DataType.Date:
+      return (value as Date).toISOString()
+    case DataType.Interval:
+      return (value as Interval).toISOStringShort()
+    case DataType.Range:
+      return stringifyRange(value as Range<any>)
+    case DataType.TypedArray:
+      value = Buffer.from(value as Uint8Array)
+    /* fallthrough */
+    case DataType.Buffer:
+      return '\\x' + (value as Buffer).toString('hex')
+    case DataType.TupleObject:
+      return stringifyTuple(
+        Object.values(value as object) as Tuple,
+        parentType,
+        quoteDepth,
+      )
+  }
+}
+
+const typeofMap: Record<string, DataType> = {
+  string: DataType.String,
+  number: DataType.Number,
+  boolean: DataType.Boolean,
+  bigint: DataType.BigInt,
+}
+
+function reflectType(value: unknown) {
   if (value == null) {
-    return 'null'
+    return DataType.Nullish
   }
   const type = typeof value
-  switch (type) {
-    case 'string':
-      return escape(value as string, type)
-    case 'number':
-    case 'boolean':
-    case 'bigint':
-      return value.toString()
-    case 'object': {
-      let obj = value as object
-      switch (obj.constructor) {
-        case Array:
-          return escape(stringifyArray(obj as any[]), 'array')
-        case Tuple:
-          return escape(
-            `(${(obj as Tuple)
-              .map(value => {
-                if (value === null) {
-                  return ''
-                }
-                return stringifyValue(value, escape)
-              })
-              .join(',')})`,
-            'tuple',
-          )
-        case Interval:
-          return escape((obj as Interval).toISOStringShort(), 'interval')
-        case Range:
-          return escape(stringifyRange(obj as Range<any>), 'range')
-        case Date:
-          return escape((obj as Date).toISOString(), 'date')
-        case RegExp:
-          return escape((obj as RegExp).source, 'pattern')
-      }
-      if (isTypedArray(obj)) {
-        obj = Buffer.from(obj)
-      }
-      if (Buffer.isBuffer(obj)) {
-        return escape('\\x' + obj.toString('hex'), 'hex')
-      }
-      if (canJsonStringify(obj)) {
-        return escape(JSON.stringify(obj), 'json')
-      }
+  if (type === 'object') {
+    switch (value.constructor) {
+      case Array:
+        return DataType.Array
+      case Tuple:
+        return DataType.Tuple
+      case Date:
+        return DataType.Date
+      case Interval:
+        return DataType.Interval
+      case Range:
+        return DataType.Range
+    }
+    if (Buffer.isBuffer(value)) {
+      return DataType.Buffer
+    }
+    if (isTypedArray(value)) {
+      return DataType.TypedArray
+    }
+    if (
+      process.env.NODE_ENV !== 'production' &&
+      tupleMarkedObjects.has(value)
+    ) {
+      return DataType.TupleObject
+    }
+    if (
+      value.constructor === undefined ||
+      value.constructor === Object ||
+      isFunction((value as any).toJSON)
+    ) {
+      return DataType.Json
+    }
+  } else {
+    const typeId = typeofMap[type]
+    if (typeId != null) {
+      return typeId
     }
   }
   throw new Error(`Unsupported type: ${type}`)
 }
 
-function canJsonStringify(obj: any): obj is { toJSON: () => string } {
-  return (
-    obj.constructor === Object ||
-    (Object.prototype.hasOwnProperty.call(obj, 'toJSON') &&
-      isFunction(obj.toJSON))
-  )
-}
-
-function stringifyArray(array: any[]) {
-  let sql = '{'
-  for (const value of array) {
-    if (sql.length > 1) {
-      sql += ','
-    }
-    sql += stringifyValue(value, escapeArrayElement)
-  }
-  return sql + '}'
-}
-
-const BACKSLASH_RE = /\\/g
-const DOUBLE_QUOTE_RE = /"/g
-const SINGLE_QUOTE_RE = /'/g
-
-function escapeArrayElement(str: string, type: EscapedType) {
-  switch (type) {
-    case 'array':
-    case 'hex':
-      return str
-    case 'json':
-      return (
-        "'" +
-        str.replace(BACKSLASH_RE, '\\\\').replace(SINGLE_QUOTE_RE, "\\'") +
-        "'"
-      )
-  }
-  return (
-    '"' +
-    str.replace(BACKSLASH_RE, '\\\\').replace(DOUBLE_QUOTE_RE, '\\"') +
-    '"'
-  )
+function quoteWrap(str: string, quoteDepth: number) {
+  // The quote may need to be escaped if it's nested in another quote-wrapped
+  // literal like an array or tuple.
+  const quote = '\\'.repeat(2 ** quoteDepth - 1) + '"'
+  return quote + str + quote
 }
--- test/core/stringify.test.ts
+++ test/core/stringify.test.ts
@@ -1,45 +1,126 @@
-import { sql } from 'pg-nano'
-import { Tuple } from 'pg-native'
+import { sql, type SQLTemplate } from 'pg-nano'
+import { cartesianProduct } from 'radashi'
+import { tupleMarkedObjects } from '../../packages/pg-native/src/value.js'
 import { getClient } from './util.js'
 
+const tuple = <T extends object>(obj: T) => {
+  tupleMarkedObjects.add(obj)
+  return obj
+}
+
 describe('stringify', () => {
-  test(
-    'string in row literal in array literal',
-    { timeout: Number.POSITIVE_INFINITY },
-    async () => {
-      const client = await getClient()
+  test('escaped literals in row/array literals', async () => {
+    const client = await getClient()
 
-      await client.query(sql`
-        CREATE TYPE foo AS (a text, b text, c text)
-      `)
+    await client.query(sql`
+      -- row -> text/json
+      CREATE TYPE foo AS (x text, y json);
+      -- row -> array -> row
+      CREATE TYPE bar AS (foo_array foo[]);
+      -- row -> row
+      CREATE TYPE foo2 AS (f foo);
+      CREATE TYPE foo3 AS (f foo2);
+      -- row -> array -> row -> row
+      CREATE TYPE bar2 AS (foo_array foo2[]);
+    `)
 
-      await client.reloadCustomTypes()
+    await client.reloadCustomTypes()
 
-      const template = sql`
-        SELECT ${sql.param([new Tuple('a', 'b', null)])}::foo[]
-      `
+    expect(
+      await client.queryValue(sql`SELECT '{"(\\"a\\",)"}'::foo[]`),
+    ).toMatchInlineSnapshot(`
+      [
+        {
+          "x": "a",
+          "y": null,
+        },
+      ]
+    `)
 
-      expect(client.stringify(template).trim()).toMatchInlineSnapshot(
-        `"SELECT $1::foo[]"`,
-      )
-      expect(template.params?.[0]).toMatchInlineSnapshot(
-        `"{"(\\"a\\",\\"b\\",)"}"`,
-      )
-      await expect(
-        client.queryRowList(template),
-      ).resolves.toMatchInlineSnapshot(`
-        [
-          {
-            "foo": [
-              {
-                "a": "a",
-                "b": "b",
-                "c": null,
-              },
-            ],
-          },
-        ]
-      `)
-    },
-  )
+    const texts = ['a', 'b']
+    const jsons = [{ a: 1, b: 2 }]
+
+    const foo = tuple({
+      x: '1',
+      y: { z: 2 },
+    })
+
+    const bar = tuple({
+      foo_array: [foo],
+    })
+
+    const foo2 = tuple({
+      f: foo,
+    })
+
+    const bar2 = tuple({
+      foo_array: [foo2],
+    })
+
+    const query =
+      (value: unknown, type: string) =>
+      (wrap: typeof sql.val): [SQLTemplate, unknown] => {
+        const template = sql`
+          SELECT ${wrap(value)}::${sql.unsafe(type)}
+        `
+
+        // Compute the template's lazy properties.
+        client.stringify(template)
+
+        return [template, value]
+      }
+
+    // We need to test both `sql.val()` and `sql.param()` because the escaping
+    // logic is different for each.
+    const queries = cartesianProduct(
+      [
+        query(texts, 'text[]'),
+        query(jsons, 'json[]'),
+        query(foo, 'foo'),
+        query([foo], 'foo[]'),
+        query(bar, 'bar'),
+        query([bar], 'bar[]'),
+        query(foo2, 'foo2'),
+        query([foo2], 'foo2[]'),
+        query(bar2, 'bar2'),
+      ],
+      [sql.val, sql.param],
+    ).map(([query, wrap]) => query(wrap as typeof sql.val))
+
+    const templates = queries.map(([template]) => template)
+
+    expect(
+      templates.map(template =>
+        template.params?.length ? template.params[0] : template.command!.trim(),
+      ),
+    ).toMatchInlineSnapshot(`
+      [
+        "SELECT '{"a","b"}'::text[]",
+        "{"a","b"}",
+        "SELECT  E'{"{\\\\"a\\\\":1,\\\\"b\\\\":2}"}'::json[]",
+        "{"{\\"a\\":1,\\"b\\":2}"}",
+        "SELECT  E'("1","{\\\\"z\\\\":2}")'::foo",
+        "("1","{\\"z\\":2}")",
+        "SELECT  E'{"(\\\\"1\\\\",\\\\"{\\\\\\\\\\\\"z\\\\\\\\\\\\":2}\\\\")"}'::foo[]",
+        "{"(\\"1\\",\\"{\\\\\\"z\\\\\\":2}\\")"}",
+        "SELECT  E'("{\\\\"(\\\\\\\\\\\\"1\\\\\\\\\\\\",\\\\\\\\\\\\"{\\\\\\\\\\\\\\\\\\\\\\\\\\\\"z\\\\\\\\\\\\\\\\\\\\\\\\\\\\":2}\\\\\\\\\\\\")\\\\"}")'::bar",
+        "("{\\"(\\\\\\"1\\\\\\",\\\\\\"{\\\\\\\\\\\\\\"z\\\\\\\\\\\\\\":2}\\\\\\")\\"}")",
+        "SELECT  E'{"(\\\\"{\\\\\\\\\\\\"(\\\\\\\\\\\\\\\\\\\\\\\\\\\\"1\\\\\\\\\\\\\\\\\\\\\\\\\\\\",\\\\\\\\\\\\\\\\\\\\\\\\\\\\"{\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"z\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\":2}\\\\\\\\\\\\\\\\\\\\\\\\\\\\")\\\\\\\\\\\\"}\\\\")"}'::bar[]",
+        "{"(\\"{\\\\\\"(\\\\\\\\\\\\\\"1\\\\\\\\\\\\\\",\\\\\\\\\\\\\\"{\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"z\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\":2}\\\\\\\\\\\\\\")\\\\\\"}\\")"}",
+        "SELECT  E'("(\\\\"1\\\\",\\\\"{\\\\\\\\\\\\"z\\\\\\\\\\\\":2}\\\\")")'::foo2",
+        "("(\\"1\\",\\"{\\\\\\"z\\\\\\":2}\\")")",
+        "SELECT  E'{"(\\\\"(\\\\\\\\\\\\"1\\\\\\\\\\\\",\\\\\\\\\\\\"{\\\\\\\\\\\\\\\\\\\\\\\\\\\\"z\\\\\\\\\\\\\\\\\\\\\\\\\\\\":2}\\\\\\\\\\\\")\\\\")"}'::foo2[]",
+        "{"(\\"(\\\\\\"1\\\\\\",\\\\\\"{\\\\\\\\\\\\\\"z\\\\\\\\\\\\\\":2}\\\\\\")\\")"}",
+        "SELECT  E'("{\\\\"(\\\\\\\\\\\\"(\\\\\\\\\\\\\\\\\\\\\\\\\\\\"1\\\\\\\\\\\\\\\\\\\\\\\\\\\\",\\\\\\\\\\\\\\\\\\\\\\\\\\\\"{\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"z\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\":2}\\\\\\\\\\\\\\\\\\\\\\\\\\\\")\\\\\\\\\\\\")\\\\"}")'::bar2",
+        "("{\\"(\\\\\\"(\\\\\\\\\\\\\\"1\\\\\\\\\\\\\\",\\\\\\\\\\\\\\"{\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"z\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\":2}\\\\\\\\\\\\\\")\\\\\\")\\"}")",
+      ]
+    `)
+
+    const result = await Promise.all(
+      templates.map(template => client.queryValue(template).catch(e => e)),
+    )
+
+    // Expect the query result to match the input values.
+    expect(result).toEqual(queries.map(([_, value]) => value))
+  })
 })
